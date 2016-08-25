36d6a699cb0cfda33bc411a0af6fa825eaa60879
DavidOConnor <david.alan.oconnor@gmail.com>
2016-08-25 12:07:23 -0500

    More work

--- quantum.py
+++ quantum.py
@@ -1,6 +1,8 @@
 from collections import namedtuple
+from functools import partial
 from typing import Tuple
 
+import matplotlib.pyplot as plt
 import numpy as np
 
 
@@ -15,22 +17,35 @@ import numpy as np
 
 # 2D grid, simulatinga a vibrating silicon/oil field. Positive values indicate
 # a height above neutral; negative below.
-GRID_SIZE = (10000, 10000)
+GRID_SIZE = (1000, 1000)
+u = np.zeros(GRID_SIZE) # velocity in x direction
+v = np.zeros(GRID_SIZE) # velocity in y direction
+x,y = mgrid[:n,:n]
+
 VIBRATION_FREQ = 1/100  # vibration cycles per tick?
 RUN_TIME = 500  # in ticks
-G = 9.81  # Gravity, in m/s^s
+G = -9.81  # Gravity, in m/s^s
+
+TICKS_PER_SECOND = 1
+
+dt = 1 # Seconds per tick
+
+PARTICLE_MASS = 1  # Assumed to be a point; ie no volume.
 
 # pos in x, y, height above neutral; respective velocities
-PARTICLE_START = (5000, 5000, 10., 0, 0, 0, 0, 0, 0)
-
-# todo include decay due to friction/drag??
-
+PARTICLE_START = (500, 500, 10., 0, 0, 0, 0, 0, -.1)  # should probably be G for z accel.
+WAVE_SPEED = 10  # m/s. Confirm this is a constant. Group vs phase velocity?
 
 # todo do I need accel? Constant down due to g, and bounce accel could be applied instantly??
 
+# negative z values are down.
+# Instead of simpler euler method step, use RK4? Either write yourself, or use scipy.integrate??
+# Perhaps use a midpoint method? (Like feynman)
+
 class Particle:
     # The particle's position (s) values are grid indices.
-    def __init__(self, sx, sy, sz, vy, vx, vz, ax, ay, az):
+    def __init__(self, mass, sx, sy, sz, vy, vx, vz, ax, ay, az):
+        self.mass = mass
         self.sx = sx
         self.sy = sy
         self.sz = sz
@@ -42,44 +57,117 @@ class Particle:
         self.az = az
 
     def step(self) -> None:
-        self.sy += self.vy
-        self.vz += self.az
-        self.sz += self.vz
+        self.sy += self.vy * dt
+        self.vz += self.az * dt
+        self.sz += self.vz * dt
 
-        self.vx += self.ax
-        self.sx += self.vx
-        self.vy += self.ay
+        self.vx += self.ax * dt
+        self.sx += self.vx * dt
+        self.vy += self.ay * dt
 
-    def bounce(self, grid: np.ndarray) -> None:
-        pass
+    def bounce(self, surface: np.ndarray) -> None:
+        """Perfectly elastic?"""
+        # todo do I need to take xz and yz into account to? Probably!
+        ke = 1/2 * self.mass * self.vz**2
+
+        # todo temp kludge!!
+        nearest_grid_point = int(round(self.sx)), int(round(self.sy))
+        self.vz *= -1.0
+        self.sz = surface.grid[nearest_grid_point[0], nearest_grid_point[1], 0]
 
 
-class Grid:
+def spatial_derivative(axis: int, A: np.ndarray):
+    """
+    Compute derivative of array A using balanced finite differences
+    Axis specifies direction of spatial derivative (d/dx or d/dy)
+
+    dA[i] =  A[i+1] - A[i-1]   / 2
+    ... or with grid spacing included ...
+    dA[i]/dx =  A[i+1] - A[i-1]   / 2dx
+
+    Used By:
+        d_dx
+        d_dy
+    """
+    grid_space = 1  # todo what is this?
+    return (np.roll(A, -1, axis) - roll(A, 1, axis)) / (grid_spacing*2.)
+
+
+d_dx = partial(spatial_derivative, 1)
+d_dy = partial(spatial_derivative, 0)
+
+
+class Surface:
     def __init__(self, size: Tuple[int, int]):
-        # grid is a n x n x 3 array containing a 2d grid of fluid height, vertical
-        # velocity, and vertical acceleration.
-        self.grid = np.zeros(*size, 3)
+        # grid is a n x n x 3 array containing a 2d grid of fluid height(pressure? η?),
+        # vertical velocity, and vertical acceleration.
+        # self.grid = np.zeros([*size, 3])
+
+        # todo one dim=3 array, or multiple dim=2
+
+        self.η = np.zeros([*size])  # Columnheight/ pressure?
+        self.dη_dt = np.zeros([*size])  # change in Columnehgith / pressure?
+
+        self.u = np.zeros([*size])  # Velocity in x direction
+        self.du_dt = np.zeros([*size])  # accel? in x direction
+
+        self.v = np.zeros([*size])  # Velocity in y direction
+        self.dv_dt = np.zeros([*size])  # accel? in y direction
+
 
     def step(self) -> None:
-        self.grid[:, :, 0] += self.grid[:, :, 1]  # Add velocity to position
-        self.grid[:, :, 1] += self.grid[:, :, 2]  # Add acceleration to velocity
+        # self.grid[:, :, 0] += self.grid[:, :, 1] * dt  # Add velocity to position
+        # self.grid[:, :, 1] += self.grid[:, :, 2] * dt  # Add acceleration to velocity
+        # self.grid[:, :, 1] += self.grid[:, :, 2] * dt  # Add acceleration to velocity
+        # self.grid[:, :, 1] += self.grid[:, :, 2] * dt  # Add acceleration to velocity
+        b = 0  # todo no idea what b is.
+        η, u, v = self.η, self.u, self.v  # Code shortener
 
-    def bounce(self, impact_point: Tuple[int, int], velocity: float=1.) -> None:
+        self.du_dt = G * d_dx(η) - b*u
+        self.dv_dt = G * d_dy(η) - b*v
+
+        H = 0 #η.mean() - our definition of η includes this term
+        self.dη_dt = -d_dx(u * (H + η)) - d_dy(v * (H + η))
+
+
+    def bounce(self, p: Particle) -> None:
+        """Perfectly elastic?"""
         # Impact point corresponds to a point on grid's first two dimensions.
-        pass
+        # todo do I need to take xz and yz into account to? Probably!
+
+        # todo interpolate!
+        nearest_grid_point = int(round(p.sx)), int(round(p.sy))
+
+        ke = 1/2 * p.mass * p.vz**2
+
+def step(particle: Particle, surface: Surface, t: int) -> Tuple[Particle, Surface, int]:
+    particle.step()
+    grid.step()
+    t += dt
+    return particle, grid, t
+
 
 
 def main():
-    grid = Grid(GRID_SIZE)
-    p = Particle(*PARTICLE_START)
+    surface = Surface(GRID_SIZE)
+    p = Particle(PARTICLE_MASS, *PARTICLE_START)
+    t = 0
 
-    for t in RUN_TIME:
-        p.step()
-        grid.step()
+    x = []
+    y = []
+    for i in range(RUN_TIME):
+        step(p, surface, t)
 
+        # print(p.sx, p.sy, p.sz, p.vx, p.vy, p.vz, p.ax, p.ay, p.az)
         # todo interpolate, instead of taking the nearest one!!
-        nearest_grid_point = int(round(p.sx)), int(round(p.sy))
 
-        if p.sz <= grid.grid[nearest_grid_point[0], nearest_grid_point[1], 0]:
-            grid.bounce(nearest_grid_point)
-            p.bounce(p, grid)
+        x.append(t)
+        y.append(p.sz)
+
+        nearest_grid_point = int(round(p.sx)), int(round(p.sy))
+        if p.sz <= surface.η[nearest_grid_point[0], nearest_grid_point[1]]:
+            surface.bounce(p)
+            p.bounce(surface)
+
+    plt.plot(x, y)
+    plt.show()
